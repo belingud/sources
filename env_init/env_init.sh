e0b9648aca786d2b4d36b821c59cd12d8b036200
belingud <im.victor@qq.com>
2020-03-06 19:55:19 +0800

    add env_init script and docker config

---
+++ env_init/env_init.sh
@@ -0,0 +1,33 @@
+
+command_exists() {
+    command "$@"
+}
+
+apt_install() {
+    if (( whoami == root))
+    then
+        apt install -y $@
+    else
+        sudo apt install -y $@
+    fi
+}
+
+check_and_install() {
+    for comm in $@
+    do
+        command_exists $comm || {
+            echo "############### $comm not found, install $comm ################"
+            apt_install $comm
+    }
+    done
+}
+
+check_and_install zsh git wget curl
+curl https://pyenv.run | bash
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# if (( uname -s == Linux))
+# then
+#     wget -P ~/downloads https://github.com/jingweno/ccat/releases/download/v1.1.0/linux-amd64-1.1.0.tar.gz
+# fi

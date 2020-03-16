373c9ec4a7d05dbec5ba7d0f12dc7453186350d2
belingud <im.victor@qq.com>
2020-03-16 22:05:54 +0800

    update django models field script

--- markdown/ModelFieldsOutPut.py
+++ markdown/ModelFieldsOutPut.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
+# author: belingud
 import django
 import os
 import sys
+from collections import OrderedDict
 from django.apps import apps
 from django.db import connection
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SantaClara.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 django.setup()
 
 args = sys.argv
@@ -13,41 +15,47 @@ args = sys.argv
 models = apps.all_models
 
 
-def get_app_models(arg, all_models):
-    if arg == 'all':
-        return all_models
-    else:
-        return all_models[arg]
+def get_app_models(arg: str, all_models: OrderedDict) -> OrderedDict:
+    """
+    arg is command line first value passed
+    """
+    return all_models if arg == "all" else all_models[arg]
 
 
-file_path = args[2] if len(args) == 3 else 'fields_detail.md'
+file_path = args[2] if len(args) == 3 else "fields_detail.md"
 
-if not file_path.endswith('.md'):
-    sys.exit('the second argument file path has to be a markdown file, now it`s {0}'.format(file_path))
+if not file_path.endswith(".md"):
+    print(
+        "the second argument file path has to be a markdown file, now it`s {0}".format(
+            file_path
+        )
+    )
+    sys.exit(1)
 
 try:
     arg = args[1]
     all_models = get_app_models(arg, models)
-    # OrderDict
+    # all_models is an OrderDict
 except IndexError:
     all_models = models.values()
 
-# print('all_models: ', all_models)
 markdown_lines = []
 
 table_name = """\n\n## {0}--{1}\n\n"""
 table_header = """| 字段名 | 数据类型 | 允许为空 | PK | 字段说明 |
 | ---- | ---- | ---- | --- | ---- |
 """
-field_desc_str = '| {0} | {1} | {2} | {3} | {4} |'
-new_line_str = '\n'
+field_desc_str = "| {0} | {1} | {2} | {3} | {4} |"
+new_line_str = "\n"
 
 
-def get_models_fields():
+def get_models_fields() -> None:
     """
-    get all models and fields, format field description by field_desc_str
-    append every field description into file_write_lines
-    and write into file if given a specific app name, and file name
+    get all models and fields,
+    format field description into field_desc_str
+    and write in file fields_detail.md by default
+    if given a specific app name, and file name,
+    write fields into a specific file
     :return:
     :rtype:
     """
@@ -55,7 +63,6 @@ def get_models_fields():
         for model in model_odict.values():
             markdown_lines.append(table_name.format(model.__name__, model._meta.db_table))
             fields_list = model._meta.get_fields()
-            ########
             markdown_lines.append(table_header)
             for field in fields_list:
                 field_name = field.name
@@ -68,16 +75,20 @@ def get_models_fields():
                 try:
                     field_verbose = field.verbose_name
                 except AttributeError:
-                    field_verbose = ' '
-                _field_str = field_desc_str.format(str(field_name), str(field_type), str(field_null),
-                                                   str(field_limit), str(field_verbose))
-                #############
-                print('_field_str: ', _field_str, end=new_line_str)
+                    field_verbose = " "
+                _field_str = field_desc_str.format(
+                    str(field_name),
+                    str(field_type),
+                    str(field_null),
+                    str(field_limit),
+                    str(field_verbose),
+                )
+                print("_field_str: ", _field_str, end=new_line_str)
                 markdown_lines.append(_field_str + new_line_str)
             markdown_lines.append(new_line_str * 2)
 
 
-def write_to_md(file_path):
+def write_to_md(file_path: str) -> None:
     """
     write fields detail into a markdown file,
     file_path could be specific
@@ -86,25 +97,30 @@ def write_to_md(file_path):
     :return:
     :rtype:
     """
-    with open(file_path, 'w') as fp:
-        print('prepare to write markdown file')
+    with open(file_path, "w") as fp:
+        print("prepare to write markdown file")
         print(len(markdown_lines))
         fp.writelines(markdown_lines)
 
 
-# python model_fields.py
-# ---> fields_detail.md
-# python model_fields.py api_home
-# ---> fields_detail.md  # include all models in api_home module
-# python models_fields.py all ~/docs/model_field.md
-# ---> ~/docs/model_field.md  # include all models and output a specific markdown file
-# python model_fields.py company company_models.md
-# ---> company_models.md  # include all models in company models and output a specific markdown file
+usage = """
+usage:
+$ python model_fields.py
+---> fields_detail.md
+$ python model_fields.py api_home
+---> fields_detail.md  # include all models in api_home module
+$ python models_fields.py all ~/docs/model_field.md
+---> ~/docs/model_field.md  # include all models and output a specific markdown file
+$ python model_fields.py company company_models.md
+---> company_models.md  # include all models in company models
+     and output a specific markdown file
+"""
 
 
-def main(file_path):
+def main(file_path: str) -> None:
     """
-    main
+    run this function by default,
+    will call the get and write method
     :param file_path:
     :type file_path:
     :return:
@@ -114,5 +130,8 @@ def main(file_path):
     write_to_md(file_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if sys.argv[1] in ("-h", "--help"):
+        print(usage)
+        sys.exit(1)
     main(file_path)

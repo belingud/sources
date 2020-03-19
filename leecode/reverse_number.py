7cc2ff19ff022184faea13a59758c0edb8c8f44a
belingud <im.victor@qq.com>
2020-03-19 22:44:15 +0800

    update

--- leecode/reverse_number.py
+++ leecode/reverse_number.py
@@ -24,44 +24,44 @@ import time
 start = time.time()
 
 
-class Solution:
-    def reverse(self, x: int) -> int:
-        """
-        字符串反转完成证书的反转
-        用pow()内置方法,相比**较快
-        """
-        # edge = pow(2, 31) - 1 if x > 0 else pow(2, 31)
-        if x < 0:
-            x = -1 * x
-            x = int("-" + str(x)[::-1])
-            # if x < edge:
-            # return 0
-        else:
-            x = int(str(x)[::-1])
-            # if x > edge:
-            # return 0
-        if x < -pow(2, 31) or x > pow(2, 31) - 1:
-            return 0
-        return x
-
-
 # class Solution:
 #     def reverse(self, x: int) -> int:
 #         """
-#         用位运算得取值范围,加快运行速度
+#         字符串反转完成证书的反转
+#         用pow()内置方法,相比**较快
 #         """
-#         y, res = abs(x), 0
-#         edge = (1 << 31) - 1 if x > 0 else 1 << 31
-#         while y != 0:
-#             res = res * 10 + y % 10
-#             if res > edge:
-#                 return 0
-#             y //= 10
-#         return res if x > 0 else -res
+#         # edge = pow(2, 31) - 1 if x > 0 else pow(2, 31)
+#         if x < 0:
+#             x = -1 * x
+#             x = int("-" + str(x)[::-1])
+#             # if x < edge:
+#             # return 0
+#         else:
+#             x = int(str(x)[::-1])
+#             # if x > edge:
+#             # return 0
+#         if x < -pow(2, 31) or x > pow(2, 31) - 1:
+#             return 0
+#         return x
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        """
+        用位运算得取值范围,没有加快运行速度?
+        """
+        y, res = abs(x), 0
+        edge = (1 << 31) - 1 if x > 0 else 1 << 31
+        while y != 0:
+            res = res * 10 + y % 10
+            if res > edge:
+                return 0
+            y //= 10
+        return res if x > 0 else -res
 
 
 s = Solution()
-# print(s.reverse(1534236469))
-print(s.reverse(123))
+print(s.reverse(1534236469))
+# print(s.reverse(123))
 
 print((time.time() - start) * 1000000)

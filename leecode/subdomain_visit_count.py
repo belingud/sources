2e5dce7807e3280ba604b50a31590c866fbd3833
belingud <1170202353@qq.com>
2020-04-08 11:18:06 +0800

    line length format

--- leecode/subdomain_visit_count.py
+++ leecode/subdomain_visit_count.py
@@ -18,7 +18,8 @@
 输入:
 ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
 输出:
-["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+["901 mail.com","50 yahoo.com","900 google.mail.com",
+"5 wiki.org","5 org","1 intel.mail.com","951 com"]
 说明:
 按照假设，会访问"google.mail.com" 900次，"yahoo.com" 50次，"intel.mail.com" 1次，"wiki.org" 5次。
 而对于父域名，会访问"mail.com" 900+1 = 901次，"com" 900 + 50 + 1 = 951次，和 "org" 5 次。

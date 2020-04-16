900cdfdc372f26b3bf85a4aa3e3ca63d998cde99
belingud <1170202353@qq.com>
2020-04-16 15:02:07 +0800

    add is plaindrome solution in leecode

---
+++ leecode/palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if x < 10:
+            return True
+        return str(x) == str(x)[::-1]

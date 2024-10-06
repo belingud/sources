b5a184554016522d9facdb26651bbfe790ffebe3
belingud <1170202353@qq.com>
2024-10-06 22:13:17 +0800

    style: reformat colorscheme.lua with consistent indentation

--- utils/config/colorscheme.lua
+++ utils/config/colorscheme.lua
@@ -1,12 +1,13 @@
+-- ~/.config/nvim/lua/plugins/colorscheme.lua
 return {
-  {
-    "sainnhe/sonokai",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_style = "andromeda"
-      vim.g.sonokai_better_performance = 1
-      vim.cmd([[colorscheme sonokai]])
-    end,
-  },
+	{
+		"sainnhe/sonokai",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.sonokai_style = "andromeda"
+			vim.g.sonokai_better_performance = 1
+			vim.cmd([[colorscheme sonokai]])
+		end,
+	},
 }
